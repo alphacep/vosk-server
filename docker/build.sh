b11fd3396686cf5d72a100a9f12fea5e1a43cd82
Nickolay Shmyrev <nshmyrev@gmail.com>
2020-04-23 10:31:25 +0200

    Unify atom servers for older cpus

--- docker/build.sh
+++ docker/build.sh
@@ -3,10 +3,10 @@
 set -e
 set -x
 
-#for kind in vosk-server ru en de cn vosk-server-en-atom vosk-server-fr-atom ; do
-#    docker build --squash --file Dockerfile.kaldi-${kind} --tag alphacep/kaldi-${kind}:latest .
-#done
+for kind in vosk-server vosk-server-atom ru en de cn en-atom fr-atom ru-atom ; do
+    docker build --squash --file Dockerfile.kaldi-${kind} --tag alphacep/kaldi-${kind}:latest .
+done
 
-for kind in vosk-server ru en de cn vosk-server-en-atom vosk-server-fr-atom ; do
+for kind in vosk-server vosk-server-atom ru en de cn en-atom fr-atom ru-atom ; do
     docker push alphacep/kaldi-${kind}:latest
 done
