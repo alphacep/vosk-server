5ba7a8f3f5ab20a842739fcb924d253fe2c109f2
Ciaran O'Reilly <ciaran@futureofvoice.com>
2021-01-15 08:22:11 +0100

    Fixes ZeroDivisionError when silence is streamed and no words could be recognized

--- grpc/stt_server.py
+++ grpc/stt_server.py
@@ -68,7 +68,7 @@ class SttServiceServicer(stt_service_pb2_grpc.SttServiceServicer):
         else:
              words = [self.get_word_info(x) for x in res.get('result', [])]
              confs = [w.confidence for w in words]
-             alt_conf = sum(confs) / len(confs)
+             alt_conf = sum(confs) / len(confs) if len(confs) > 0 else 0
              alternatives = [stt_service_pb2.SpeechRecognitionAlternative(text=res['text'], words=words, confidence=alt_conf)]
              chunks = [stt_service_pb2.SpeechRecognitionChunk(alternatives=alternatives, final=True)]
              return stt_service_pb2.StreamingRecognitionResponse(chunks=chunks)
