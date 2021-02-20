2b328de5f1cb48ec6da8caa974104bc5ac438ed9
milochen0418 <milo_chen@mail2000.com.tw>
2021-02-20 18:04:59 +0800

    Support WebRTC feature done

--- webrtc/asr_server_webrtc.py
+++ webrtc/asr_server_webrtc.py
@@ -29,18 +29,24 @@ loop = asyncio.get_event_loop()
 
 def process_chunk(rec, message):
     if rec.AcceptWaveform(message):
+        o = json.loads(rec.Result())
+        if 'result' in o.keys():
+            return '{"text":"' +o['text']+ '"}'
         return rec.Result()
     else:
         return rec.PartialResult()
 
+dataframes = bytearray(b"")
+
 class KaldiTask:
     def __init__(self, user_connection):
-        self.__resampler = AudioResampler(format='s16', layout='mono', rate=8000)
+        self.__resampler = AudioResampler(format='s16', layout='mono', rate=48000)
         self.__pc = user_connection
         self.__audio_task = None
         self.__track = None
         self.__channel = None
         self.__recognizer = KaldiRecognizer(model, 48000)
+
 
     async def set_audio_track(self, track):
         self.__track = track
@@ -59,12 +65,20 @@ class KaldiTask:
     async def __run_audio_xfer(self):
         while True:
             frame = await self.__track.recv()
-            print ("Got frame", frame)
             frame = self.__resampler.resample(frame)
             data = frame.to_ndarray()
-            response = await loop.run_in_executor(pool, process_chunk, self.__recognizer, data.tobytes())
-            print ("Sending response", response)
-            self.__channel.send(response)
+
+            max_frames_len = 10000
+            global dataframes
+            message = data.tobytes()
+            recv_frames = bytearray(message)
+            dataframes += recv_frames
+            if len(dataframes) > max_frames_len:
+                wave_bytes = bytes(dataframes)
+                response = await loop.run_in_executor(pool, process_chunk, self.__recognizer, wave_bytes)
+                print(response)
+                self.__channel.send(response)
+                dataframes = bytearray(b"")
 
 
 async def index(request):
