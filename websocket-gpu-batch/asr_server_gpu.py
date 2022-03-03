dd05c141cdfcdfe69ae0076b7971af9f899b4108
Nickolay Shmyrev <nshmyrev@gmail.com>
2022-03-03 21:17:26 +0100

    Refactor websocket server to new GPU API

--- websocket-gpu-batch/asr_server_gpu.py
+++ websocket-gpu-batch/asr_server_gpu.py
@@ -8,55 +8,64 @@ import pathlib
 import websockets
 import logging
 
-from vosk import BatchRecognizer, GpuInit
+from vosk import BatchModel, BatchRecognizer, GpuInit
 
 
 async def recognize(websocket, path):
     global args
     global loop
     global pool
-    global rec
-    global client_cnt
+    global model
 
-    uid = client_cnt
-    client_cnt += 1
+    rec = None
 
-    logging.info('Connection %d from %s', uid, websocket.remote_address);
+    logging.info(f'Connection from {websocket.remote_address}');
 
     while True:
 
         message = await websocket.recv()
 
+        # Load configuration if provided
+        if isinstance(message, str) and 'config' in message:
+            jobj = json.loads(message)['config']
+            logging.info("Config %s", jobj)
+            if 'sample_rate' in jobj:
+                sample_rate = float(jobj['sample_rate'])
+            continue
+
+        # Create the recognizer, word list is temporary disabled since not every model supports it
+        if not rec:
+            rec = BatchRecognizer(model, sample_rate)
+
         if message == '{"eof" : 1}':
-            rec.FinishStream(uid)
+            rec.FinishStream()
             break
 
         if isinstance(message, str) and 'config' in message:
             continue
 
-        rec.AcceptWaveform(uid, message)
+        rec.AcceptWaveform(message)
 
-        while rec.GetPendingChunks(uid) > 0:
+        while rec.GetPendingChunks() > 0:
             await asyncio.sleep(0.1)
 
-        res = rec.Result(uid)
+        res = rec.Result()
         if len(res) == 0:
             await websocket.send('{ "partial" : "" }')
         else:
             await websocket.send(res)
 
-    while rec.GetPendingChunks(uid) > 0:
+    while rec.GetPendingChunks() > 0:
         await asyncio.sleep(0.1)
 
-    res = rec.Result(uid)
+    res = rec.Result()
     await websocket.send(res)
 
 def start():
 
-    global rec
+    global model
     global args
     global loop
-    global client_cnt
 
     # Enable loging if needed
     #
@@ -72,9 +81,7 @@ def start():
 
     GpuInit()
 
-    rec = BatchRecognizer()
-
-    client_cnt = 0
+    model = BatchModel()
 
     loop = asyncio.get_event_loop()
 
