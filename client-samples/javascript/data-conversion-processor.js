4b3f97c7b95fb0dc9ace6f3c83fab7cd8f30e1ec
ivan provalov <iprovalo@yahoo.com>
2022-03-22 15:52:44 -0700

    Update data-conversion-processor.js
    Adding buffer to the AudioWorkletProcessor

--- client-samples/javascript/data-conversion-processor.js
+++ client-samples/javascript/data-conversion-processor.js
@@ -1,19 +1,48 @@
 class DataConversionAudioProcessor extends AudioWorkletProcessor {
+    bufferSize = 4096
+    _bytesWritten = 0
+    _buffer = new Int16Array(this.bufferSize)
 
-    constructor(options){
+    constructor(options) {
         super()
+        this.initBuffer()
+    }
+
+    initBuffer() {
+        this._bytesWritten = 0
+    }
+
+    isBufferEmpty() {
+        return this._bytesWritten === 0
+    }
+
+    isBufferFull() {
+        return this._bytesWritten === this.bufferSize
     }
 
     process(inputs, outputs, parameters) {
         const inputData = inputs[0][0];
-        if (inputData) {
-            const targetBuffer = new Int16Array(inputData.length);
-            for (let index = inputData.length; index > 0; index--) {
-                targetBuffer[index] = 32767 * Math.min(1, inputData[index]);
-            }
-            this.port.postMessage(targetBuffer.buffer);
+
+        if (this.isBufferFull()) {
+            this.flush()
         }
+
+        if (!inputData) return
+
+        for (let index = inputData.length; index > 0; index--) {
+            this._buffer[this._bytesWritten++] = 32767 * Math.min(1, inputData[index]);
+        }
+
         return true;
+    }
+
+    flush() {
+        this.port.postMessage(
+            this._bytesWritten < this.bufferSize
+                ? this._buffer.slice(0, this._bytesWritten)
+                : this._buffer
+        )
+        this.initBuffer()
     }
 }
 
