b0da05b5c367f1327e07cff3dd645e01051fb083
Nickolay Shmyrev <nshmyrev@gmail.com>
2023-04-06 01:53:51 +0200

    Add Hindi

--- docker/build.sh
+++ docker/build.sh
@@ -5,10 +5,10 @@ set -x
 
 docker build --no-cache --build-arg KALDI_MKL=0 --file Dockerfile.kaldi-vosk-server --tag alphacep/kaldi-vosk-server:latest .
 
-for kind in ru en de cn fr es en-in grpc-en en-spk ja; do
+for kind in ru en de cn fr es en-in grpc-en en-spk ja hi; do
     docker build --file Dockerfile.kaldi-${kind} --tag alphacep/kaldi-${kind}:latest .
 done
 
-for kind in vosk-server ru en de cn fr es en-in grpc-en en-spk ja; do
+for kind in vosk-server ru en de cn fr es en-in grpc-en en-spk ja hi; do
     docker push alphacep/kaldi-${kind}:latest
 done
