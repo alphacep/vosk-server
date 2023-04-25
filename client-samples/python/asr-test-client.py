b60de1024ec0b91af2b7aa58f151a2e8513808da
Nickolay Shmyrev <nshmyrev@gmail.com>
2023-04-25 16:27:24 +0200

    Add websocket-client version

---
+++ client-samples/python/asr-test-client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import wave
+import sys
+
+from websocket import create_connection
+
+ws = create_connection("ws://localhost:2700")
+
+wf = wave.open(sys.argv[1], "rb")
+ws.send('{ "config" : { "sample_rate" : %d } }' % (wf.getframerate()))
+buffer_size = int(wf.getframerate() * 0.2) # 0.2 seconds of audio
+while True:
+    data = wf.readframes(buffer_size)
+
+    if len(data) == 0:
+        break
+
+    ws.send_binary(data)
+    print (ws.recv())
+
+ws.send('{"eof" : 1}')
+print (ws.recv())
