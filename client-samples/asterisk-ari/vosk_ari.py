beea2e6acf82e680c6a7dad88f1aa1aa3bada7bb
Nickolay Shmyrev <nshmyrev@gmail.com>
2023-08-17 09:13:49 +0200

    Allow multiple calls in statis

--- client-samples/asterisk-ari/vosk_ari.py
+++ client-samples/asterisk-ari/vosk_ari.py
@@ -4,8 +4,10 @@ import anyio
 import asyncari
 import logging
 import aioudp
-
 import os
+import vosk
+import array
+
 ast_host = os.getenv("AST_HOST", '127.0.0.1')
 ast_port = int(os.getenv("AST_ARI_PORT", 8088))
 ast_url = os.getenv("AST_URL", 'http://%s:%d/'%(ast_host,ast_port))
@@ -13,21 +15,32 @@ ast_username = os.getenv("AST_USER", 'asterisk')
 ast_password = os.getenv("AST_PASS", 'asterisk')
 ast_app = os.getenv("AST_APP", 'hello-world')
 
-import vosk
 
 model = vosk.Model(lang='en-us')
-recognizer = vosk.KaldiRecognizer(model, 16000)
+channels = {}
 
-import array
+class Channel:
 
-async def rtp_handler(connection):
-    async for message in connection:
-        data = array.array('h', message[12:])
-        data.byteswap()
-        if recognizer.AcceptWaveform(data.tobytes()):
-               print (recognizer.Result())
-        else:
-               print (recognizer.PartialResult())
+    async def rtp_handler(self, connection):
+        async for message in connection:
+            data = array.array('h', message[12:])
+            data.byteswap()
+            if self.rec.AcceptWaveform(data.tobytes()):
+                res = self.rec.Result()
+            else:
+                res = self.rec.PartialResult()
+            print(res)
+
+    async def init(self, client, channel):
+        self.port = 45000 + len(channels)
+        self.rec = vosk.KaldiRecognizer(model, 16000)
+        self.udp = aioudp.serve("localhost", self.port, self.rtp_handler)
+        await self.udp.__aenter__()
+
+        bridge = await client.bridges.create(type='mixing')
+        media_id = client.generate_id()
+        await client.channels.externalMedia(channelId=media_id, app=client._app, external_host='localhost:' + str(self.port), format='slin16')
+        await bridge.addChannel(channel=[media_id, channel.id])
 
 async def statis_handler(objs, ev, client):
     channel = objs['channel']
@@ -35,20 +48,15 @@ async def statis_handler(objs, ev, client):
     if 'UnicastRTP' in channel.name:
          return
 
-    bridge = await client.bridges.create(type='mixing')
-
-    media_id = client.generate_id()
-    await client.channels.externalMedia(channelId=media_id, app=client._app, external_host='localhost:45000', format='slin16')
-
-    await bridge.addChannel(channel=[media_id, channel.id])
-
+    local_channel = Channel()
+    await local_channel.init(client, channel)
+    channels[channel.id] = local_channel
 
 async def main():
      async with asyncari.connect(ast_url, ast_app, ast_username,ast_password) as client:
-        async with aioudp.serve("localhost", 45000, rtp_handler) as udpserv:
-             async with client.on_channel_event('StasisStart') as listener:
-                 async for objs, event in listener:
-                      await statis_handler(objs, event, client)
+         async with client.on_channel_event('StasisStart') as listener:
+             async for objs, event in listener:
+                  await statis_handler(objs, event, client)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
