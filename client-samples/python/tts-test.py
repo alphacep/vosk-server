6ad475dcc9f24d9153edba01d01a85731992fdd8
Nickolay Shmyrev <nshmyrev@gmail.com>
2023-12-13 16:52:17 +0100

    Remove old tts clients

--- client-samples/python/tts-test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-import requests
-
-r = requests.post("https://api.alphacephei.com/tts", data={'text' : "Hello world!"})
-fn = open("tts.wav", "wb")
-fn.write(r.content)
-fn.close()
