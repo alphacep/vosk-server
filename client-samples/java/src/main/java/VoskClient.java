56e8017c4a3c9ccca1d1d425205f508fdf344aad
Pablo J. Rogina <43066+pablojr@users.noreply.github.com>
2024-04-07 23:19:18 -0300

    Reverted hostname

--- client-samples/java/src/main/java/VoskClient.java
+++ client-samples/java/src/main/java/VoskClient.java
@@ -14,7 +14,7 @@ public class VoskClient {
 
     public ArrayList<String> transcribe(String path) throws Exception {
             WebSocketFactory factory = new WebSocketFactory();
-            WebSocket ws = factory.createSocket("ws://a4v-rpi.local:2700");
+            WebSocket ws = factory.createSocket("ws://localhost:2700");
             ws.addListener(new WebSocketAdapter() {
                 @Override
                 public void onTextMessage(WebSocket websocket, String message) {
