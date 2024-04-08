5db5b0e89473973f9cd9055d44e48805cb875655
Pablo J. Rogina <43066+pablojr@users.noreply.github.com>
2024-04-08 13:11:05 -0300

    Changed example reading file to actually connect to Vosk server (#250)
    * Changed example to actually connect to Vosk server
    
    * Reverted hostname

--- client-samples/java/src/main/java/VoskClient.java
+++ client-samples/java/src/main/java/VoskClient.java
@@ -3,6 +3,9 @@ import java.io.*;
 import java.nio.file.*;
 import java.util.concurrent.*;
 import java.util.ArrayList;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
 
 public class VoskClient {
 
@@ -21,23 +24,47 @@ public class VoskClient {
             });
             ws.connect();
 
-            FileInputStream fis = new FileInputStream(new File(path));
-            DataInputStream dis = new DataInputStream(fis);
-            byte[] buf = new byte[8000];
-            while (true) {
-                int nbytes = dis.read(buf);
-                if (nbytes < 0) break;
-                recieveLatch = new CountDownLatch(1);
-                ws.sendBinary(buf);
-                recieveLatch.await();
-            }
-            recieveLatch = new CountDownLatch(1);
-            ws.sendText("{\"eof\" : 1}");
-            recieveLatch.await();
-            ws.disconnect();
+            int totalFramesRead = 0;
+            File fileIn = new File(path);
+            try {
+                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(fileIn);
+                AudioFormat format = audioInputStream.getFormat();
+                int bytesPerFrame = format.getFrameSize();
+                if (bytesPerFrame == AudioSystem.NOT_SPECIFIED) {
+                    bytesPerFrame = 1;
+                }
 
+                // Let Vosk server now the sample rate of sound file
+                ws.sendText("{ \"config\" : { \"sample_rate\" : " + (int)format.getSampleRate() + " } }");
+
+                // Set an arbitrary buffer size of 1024 frames.
+                int numBytes = 1024 * bytesPerFrame;
+                byte[] audioBytes = new byte[numBytes];
+                try {
+                    int numBytesRead = 0;
+                    int numFramesRead = 0;
+                    // Try to read numBytes bytes from the file.
+                    while ((numBytesRead = audioInputStream.read(audioBytes)) != -1) {
+                        // Calculate the number of frames actually read.
+                        numFramesRead = numBytesRead / bytesPerFrame;
+                        totalFramesRead += numFramesRead;
+                        recieveLatch = new CountDownLatch(1);
+                        ws.sendBinary(audioBytes);
+                        recieveLatch.await();
+                    }
+                    recieveLatch = new CountDownLatch(1);
+                    ws.sendText("{\"eof\" : 1}");
+                    recieveLatch.await();
+                    ws.disconnect();
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+		}
+            } catch (Exception e) {
+                e.printStackTrace();
+	    }
             return results;
-    }
+        }
 
     public static void main(String[] args) throws Exception {
         VoskClient client = new VoskClient();
